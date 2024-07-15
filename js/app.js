cbfd0add858192781a538b0afbcf565ea6839fce
Sebastian <sebastianmurphy516@gmail.com>
2024-07-15 14:25:08 -0400

    Hundo complete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,34 @@
-function getValues(){
-    let messageInput = document.getElementById("message").value;
-    let alertMessage = document.getElementById("alertMessage");
-    alertMessage.innerText = messageInput;
+function getValues() {
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+    if(Number.isInteger(startValue) && Number.isInteger(endValue)){
+        let numbers = generateNumbers(startValue, endValue);
+        displayNumbers(numbers);
+    } else {
+        alert("You must enter a number.");
+    }
+}
+function generateNumbers(start, end) {
+    let numbers = [];
+    for(i = start; i <= end; i++){
+        numbers.push(i);
+    }
+    return numbers;
+}
+function displayNumbers(numbers) {
+    // Array index starts at zero so final element is length-1
+    let templateCols = "";
+    for(let i=0; i < numbers.length; i++) {
+        let classname = "";
+        if(numbers[i] % 2 == 0) {
+            let classname = "even";
+        templateCols += `<div class="col ${classname}">${numbers[i]},</div>`;
+        } else {
+            let classname = "odd";
+        templateCols += `<div class="col ${classname}">${numbers[i]},</div>`;
+        }
+    }
+    document.getElementById("outputData").innerHTML = templateCols;
 }
